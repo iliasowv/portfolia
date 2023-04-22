e38dfdc1501d5fcef3faff80b3b7cf258c52b97f
iliasowv <130992462+iliasowv@users.noreply.github.com>
2023-04-22 15:07:09 -0700

    Add files via upload

---
+++ сайт/java.js
@@ -0,0 +1,515 @@
+@media  screen and (max-width:1600px){
+    .img-b3{
+        width: 450px;
+        height: 400px;
+    }
+    .photo-2{
+        width: 100%;
+        height: 270px ;
+        
+    }
+
+    .img{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        position: relative;
+        
+    }
+
+    .member-2{
+        position: relative;      
+        padding: 0 5px;
+        transition: .5s all ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative; 
+    }
+
+    .description-2{      
+        margin: 0 10px;
+        position: absolute;
+        color: #000000;
+         text-decoration: none;
+        color: #fff;
+        font-family: sans-serif;
+        width: 70%;
+        height: 100%;
+        text-align: center;
+        background-color:#C76637;
+        user-select: none;
+        opacity: 0.4;
+        transition: .3;
+        background: #333;
+     }
+
+     .prof-2{
+        color: hsl(0deg 0% 100% / 0%);
+        font-weight: bold;
+    }
+
+}
+
+@media  screen and (max-width:1400px){
+
+    .photo-2{
+        width: 100%;
+        height: 270px ;
+        
+    }
+
+    .img{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        position: relative;
+        
+    }
+
+    .member-2{
+        position: relative;      
+        padding: 0 5px;
+        transition: .5s all ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative; 
+    }
+
+    .description-2{      
+        margin: 0 10px;
+        position: absolute;
+        color: #000000;
+         text-decoration: none;
+        color: #fff;
+        font-family: sans-serif;
+        width: 80%;
+        height: 100%;
+        text-align: center;
+        background-color:#C76637;
+        user-select: none;
+        opacity: 0.4;
+        transition: .3;
+        background: #333;
+     }
+
+     .prof-2{
+        color: hsl(0deg 0% 100% / 0%);
+        font-weight: bold;
+    }
+
+}
+  
+
+@media  screen and (max-width:1250px){
+
+    .history-title{
+    font-family: 'Open Sans', sans-serif;
+    font-family: 'Tinos', serif;
+    font-size: 50px; 
+    font-weight:400;
+    width: 444px;
+    text-align: center;
+    margin-left: 26%;
+}
+
+.history-desc{
+    color:#656363 ;
+    font-family: 'Open Sans', sans-serif;
+    font-family: 'Tinos', serif;
+    font-size: 20px;
+    width: 444px;
+    text-align: center;
+    margin-top: 24px;
+    margin-left: 26%;
+    
+}
+
+    .produkt{
+        background-image: url(fonnn.jpg);
+        height: 100vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin-top: 5%;
+        
+    }
+
+    .produkt{
+        background-color: #a09f9f04;
+        
+    }
+
+    .produkts{
+      position: relative;    
+      display: flex;
+      margin-top: 2%; 
+      justify-content: space-around;
+      
+      margin-left: 10%;
+    
+    
+  }
+   
+     .member{
+        position: relative;
+        max-width: 300px;
+        padding: 0 5px;
+        transition: .5s all ease;
+        display: flex;
+        align-items: baseline;
+    
+    }
+    
+    .photo{
+        width:100%;
+       
+        
+    }
+
+    .description{
+       
+       margin: 0 10px;
+       position: absolute;
+       color: #000000;
+        text-decoration: none;
+       color: #fff;
+       font-family: sans-serif;
+       width: 100%;
+       height: 100%;
+       text-align: center;
+       background-color:#C76637;
+       user-select: none;
+       opacity: 0.4;
+       transition: .3;
+       background: rgba(255,255,255,0.5);
+    }
+    
+    .name{
+        font-weight: bold;
+        font-size: 35px;
+        line-height: 0;
+        margin-top: 70%;
+        color: #000000;
+        text-decoration: none;
+    }
+
+    .silk{
+    
+        color: #000000;
+        text-decoration: none; 
+        border-left: 3px solid #000000 ;
+        border-right:  3px solid #000000;
+        transition: color 0.3s linear;
+    }
+
+    .silk:hover{
+        color: #1401317e;
+    }
+
+    .prof{
+        color: #000000;
+        font-weight: bold;
+    }
+    
+    .member:hover{
+        margin-top: -40px;
+        opacity: 1;
+    }
+    
+    .member:hover .description{
+        margin-top: -40px;
+        opacity: 1;
+    }
+
+    .photo-2{
+        width: 100%;
+        height: 270px ;
+        
+    }
+
+    .img{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        position: relative;
+        
+    }
+
+    .member-2{
+        position: relative;      
+        padding: 0 5px;
+        transition: .5s all ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative; 
+    }
+
+    .description-2{      
+        margin: 0 10px;
+        position: absolute;
+        color: #000000;
+         text-decoration: none;
+        color: #fff;
+        font-family: sans-serif;
+        width: 90%;
+        height: 100%;
+        text-align: center;
+        background-color:#C76637;
+        user-select: none;
+        opacity: 0.4;
+        transition: .3;
+        background: #333;
+     }
+
+     .prof-2{
+        color: hsl(0deg 0% 100% / 0%);
+        font-weight: bold;
+    }
+
+}
+
+@media  screen and (max-width:1100px){
+    
+
+    .photo-2{
+        width: 100%;
+        height: 270px ;
+        
+    }
+
+    .img{
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        position: relative;
+        
+    }
+
+    .member-2{
+        position: relative;      
+        padding: 0 5px;
+        transition: .5s all ease;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative; 
+    }
+
+    .description-2{      
+        margin: 0 10px;
+        position: absolute;
+        color: #000000;
+         text-decoration: none;
+        color: #fff;
+        font-family: sans-serif;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        background-color:#C76637;
+        user-select: none;
+        opacity: 0.4;
+        transition: .3;
+        background: #333;
+     }
+
+     .prof-2{
+        color: hsl(0deg 0% 100% / 0%);
+        font-weight: bold;
+    }
+        
+}     
+        
+
+
+@media  screen and (max-width:1000px){ 
+
+    .photo-2{
+        width: 100%;
+        height:31vh  ;
+        
+    }
+
+    .produkt{
+        background-image: url(fonnn.jpg);
+        height: 80vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+        margin-top: 5%;
+        
+    }
+
+    .header{
+        background-image: url(это.jpg);
+        height: 90vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .titless{
+        margin-top: 20px;
+        align-items: center;
+        color: #000000;
+        font-size: 40px;
+        text-align: center;
+        margin-right: 5%;
+    
+    }
+    
+    .pro{
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+        font-size: 40px; 
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        margin-right: 8%;    
+    }
+    
+    .zakaz{
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+        font-size: 20px; 
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        margin-right: 7%; 
+    }
+}
+
+@media only screen and (max-width:999px){
+    .history-title{
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+        font-size: 30px; 
+        text-align: center;
+        
+    }
+    
+    .histore-info{
+        background: white;
+      border-radius: 6px;
+      color: #000000;
+      padding-inline-start: 16px;
+      width: 560px;
+      overflow: hidden;
+      transition: .6s;
+      transform: translate(0);
+      font-weight: 400;
+      padding: 30px;
+      margin-left: 10%;
+      text-align: center;
+  }
+
+  .histore-info::before{
+    content: '';
+    position: absolute;
+    top:-75px;
+    left: -45px;
+    z-index: -1;
+    background: linear-gradient(to right, #3bd9d9, #36a6bf);
+    border-radius: 50%;
+    width: 276px;
+    height: 265px;
+    transition: .6s;
+}
+
+    .card-title{
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+        font-size: 25px; 
+        font-weight:500;
+    
+    }
+    .card{
+    
+        width: 250px;
+        box-shadow: 10px 10px 40px 20px #797a76;
+        margin-right: 31px;
+        background-color: rgb(255, 255, 255);
+        margin-top:-70px;
+        padding: 0px 10px;
+    
+    }
+
+    .nav-item{
+        color: rgb(0, 0, 0);
+        text-decoration: none;
+        font-weight: 700;
+        font-size: 20px;
+        margin-right: 25px;
+        border-left: 3px solid #000000 ;
+        border-right:  3px solid #000000;
+        transition: color 0.3s linear;
+       
+    }
+
+    .header-title {
+        color:black;
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+        font-size: 40px; 
+        font-weight:500;
+        text-align: center;
+        justify-content: center;
+        margin-top: 2%;    
+        margin-right: 7%;
+        
+    
+    }
+
+    .title{
+        margin-top: 20px;
+        align-items: center;
+        color: #000000;
+        font-size: 40px;
+        text-align: center;
+        margin-right: 8%;
+
+    }
+
+    .o_comapani{
+        margin-top: 2%;
+        text-align: center;
+        align-items: center;
+        display: flex;
+        position: relative;
+        font-size: 25px;
+        justify-content: center;
+        width: 80%;   
+        margin-left: 10%; 
+        font-family: 'Open Sans', sans-serif;
+        font-family: 'Tinos', serif;
+
+    }
+
+    .titless{
+        margin-top: 20px;
+        align-items: center;
+        color: #000000;
+        font-size: 40px;
+        text-align: center;
+        margin-right: 5%;
+    
+    }
+
+    .header{
+        background-image: url(это.jpg);
+        height: 95vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+ }
+
+ @media only screen and (max-width:880px){
+    .header{
+        background-image: url(это.jpg);
+        height: 80vh;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+ }
